6db3726ff546aecb65a84ebcaada1dae4c0b6209
Pekemi01 <pekemi01@thu.de>
2026-05-09 18:49:34 +0200

    Environment variables hinzugefügt und development cache Dateien generiert

--- my-app/lib/actions/massnahmen.js
+++ my-app/lib/actions/massnahmen.js
@@ -297,3 +297,4 @@ export async function searchMeasuresByName(measureName) {
     databaseClient.release();
   }
 }
+
--- my-app/lib/db.js
+++ my-app/lib/db.js
@@ -7,17 +7,15 @@ import { Pool } from 'pg';
 
 
 // Verhindert, dass in der Entwicklung bei jedem Speichern ein neuer Pool erstellt wird
-let db;
-
 if (!global.pool) {
   global.pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    // Optionale Optimierung für Prototypen:
-    max: 10, // Maximal 10 gleichzeitige Verbindungen
+    max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
   });
 }
-pool = global.pool;
 
-export default pool;
+const db = global.pool;
+
+export default db;
